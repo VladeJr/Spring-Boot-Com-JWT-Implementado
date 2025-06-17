0bb2bf72bca70b6f715fed2f2b39896099aed0a7
Vlade <nekaytsu@gmail.com>
2025-06-16 22:05:41 -0300

    feat: Configurando Spring Security com JWT e controle de acesso baseado em roles

--- jwtapp/src/main/java/com/jwtspring/jwtapp/security/JwtUtil.java
+++ jwtapp/src/main/java/com/jwtspring/jwtapp/security/JwtUtil.java
@@ -3,6 +3,7 @@ package com.jwtspring.jwtapp.security;
 import com.jwtspring.jwtapp.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,8 +11,8 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String jwtSecret = "secreto123"; // Substitua por um segredo seguro
-    private final long jwtExpirationMs = 86400000; // 24 horas
+    private final String jwtSecret = "secreto123";
+    private final long jwtExpirationMs = 86400000;
 
     public String generateToken(User user) {
         return Jwts.builder()
@@ -21,5 +22,19 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
     }
 }
